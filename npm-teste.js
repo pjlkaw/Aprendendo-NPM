de926617fcf5c8c7e99797fdace25f35a9125ea2
pjlkaw <pedrorochabispo@gmail.com>
2025-11-17 22:06:35 -0300

    Menu de escolhas em desenvolvimento

--- npm-teste.js
+++ npm-teste.js
@@ -8,7 +8,8 @@ console.log(
   )
 );
 
-//   const random = Math.floor(Math.random() * 10) + 1;
+// Fazer um menu de escolha com inquirer
+// const random = Math.floor(Math.random() * 10) + 1;
 
 async function buscaUsuarios(id) {
   try {
@@ -22,6 +23,7 @@ async function buscaUsuarios(id) {
     return data;
   } catch {
     console.log(chalk.red("Erro"));
+    return null;
   }
 }
 
@@ -29,31 +31,52 @@ async function iniciar() {
   for (let i = 1; i <= 10; i++) {
     await buscaUsuarios(i);
   }
-  const { number } = await inquirer.prompt([
+
+  const escolha = await inquirer.prompt([
     {
-      type: "input",
-      name: "number",
-      message:
-        "Escolha o número de algum usuário para inspecionar (entre 1 e 10) ..: ",
-      validate(value) {
-        const num = Number(value);
-        return num >= 1 && num <= 10 ? true : "Isso não é um número de 1 a 10";
+      type: 'rawlist',
+      name: 'escolha',
+      message: 'Escolha o que quer fazer:',
+      choices: [
+        { name: 'Busca por nome', value: '1'},
+        { name: 'Busca por ID', value: '2'}
+      ]
+    }
+  ])
+  if (escolha.value !== '1' && escolha.value != '2') {
+    console.log("Nenhuma das alternativas")
+    return
+  }
+  else if (escolha.value == '1') {
+    console.log('Função em desenvolvimento')
+    return
+  }
+  else if (escolha.value == '2') {
+    const { number } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "number",
+        message:
+          "Escolha o número de algum usuário para inspecionar (entre 1 e 10) ..: ",
+        validate(value) {
+          const num = Number(value);
+          return num >= 1 && num <= 10 ? true : "Isso não é um número de 1 a 10";
+        },
       },
-    },
-  ]);
-
-  const user = await buscaUsuarios(Number(number));
-
-  if (user) {
-    console.log(chalk.red(" USER ID - " + number));
-    console.log(chalk.bgGray(chalk.hex("#FFE922")(` Nome: ${user.name} `)));
-    console.log(chalk.bgGray(chalk.hex("#4EFF22")(` Usuário: ${user.username} `)));
-    console.log(chalk.bgGray(chalk.hex("#22FFCF")(` Email: ${user.email} `)));
-    console.log(chalk.bgGray(chalk.hex("#4A22FF")(` Telefone: ${user.phone} `)));
-    console.log(chalk.bgGray(chalk.hex("#FF22DA")(` Website: ${user.website} `)));
-    console.log(chalk.red("==========================="));
-    console.log(chalk.red("FIM DO PROGRAMA"));
+    ]);
+  
+    const user = await buscaUsuarios(Number(number));
+  
+    if (user) {
+      console.log(chalk.red(" USER ID - " + number));
+      console.log(chalk.bgGray(chalk.hex("#FFE922")(` Nome: ${user.name} `)));
+      console.log(chalk.bgGray(chalk.hex("#4EFF22")(` Usuário: ${user.username} `)));
+      console.log(chalk.bgGray(chalk.hex("#22FFCF")(` Email: ${user.email} `)));
+      console.log(chalk.bgGray(chalk.hex("#4A22FF")(` Telefone: ${user.phone} `)));
+      console.log(chalk.bgGray(chalk.hex("#FF22DA")(` Website: ${user.website} `)));
+      console.log(chalk.red("==========================="));
+      console.log(chalk.red("FIM DO PROGRAMA"));
+    }
   }
 }
-
 iniciar();
